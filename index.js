aec0ec268585249e11f5bb989a7432eeabbb8bd1
BLemine <baillahiamine@gmail.com>
2020-01-17 05:07:39 +0100

    v0.1

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const ctx=cvs.getContext("2d");
 
 const bird=new Image();
 const bg=new Image();
-var fg = new Image();
-var pipeNorth = new Image();
-var pipeSouth = new Image();
+const fg = new Image();
+const pipeNorth = new Image();
+const pipeSouth = new Image();
+
+let birdX=10;
+let birdY=150;
+
+let flyMusic = new Audio();
+let scoreMusic = new Audio();
 
 bird.src="bird.png";
 bg.src = "bg.png";
@@ -14,7 +20,30 @@ pipeNorth.src = "pipeNorth.png";
 pipeSouth.src = "pipeSouth.png";
 
 ctx.drawImage(bg,0,0);
-ctx.drawImage(bird,10,150);
+ctx.drawImage(bird,birdX,birdY);
+ctx.drawImage(pipeNorth,130,0);
+ctx.drawImage(pipeSouth,130,330);
+
+
+const mvdown=()=>{
+	birdY-=25;
+}
+document.addEventListener("keydown",mvdown);
+
+
+const startGame=()=>{
+	birdX+=10;
+	
+	ctx.drawImage(fg,0,cvs.height - fg.height);
+	requestAnimationFrame(startGame);
+}
+
+startGame();
+/*setInterval(()=>{
+	birdX+=10;
+	ctx.clearRect(10,160,10,150)
+	ctx.drawImage(bird,birdX,birdY);
+},100);*/
 
 
 
